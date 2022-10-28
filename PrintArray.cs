fc5015e7ea6bff6f819619af8fa9c3a4625310b7
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-28 23:26:39 +0530

    Added print array by extending parameters

---
+++ PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics;
+
+namespace GenericsProblem
+{
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toprint()
+        {
+              foreach (var ele in inputArray)
+              {
+                Console.WriteLine(ele);
+              }
+        }
+    }
+}
