b1f32894a0266250e69d7efdfdca2fd820ee3668
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-28 23:24:02 +0530

    Added refactor to the problem using generics method

---
+++ Refactor1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    public class Refactor1<T> where T : IComparable 
+    {
+        public T first, second, third;
+        public Refactor1  (T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public static T MaximumNumber(T first, T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+            return first;
+        }
+        public T MaxMethod()
+        {
+            T max = Refactor1<T>.MaximumNumber(first, second, third);
+            return max;
+        }
+          
+        
+        
+    }
+    
+
+}
