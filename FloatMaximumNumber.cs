99883e95df3ce7e21e45ea92bafb4c9b721a1503
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-28 23:20:40 +0530

    Added float maximum number among three

---
+++ FloatMaximumNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class FloatMaximumNumber
+    {
+        public static double MaximumFloatNumber(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+            return first;
+        }
+    }
+}
